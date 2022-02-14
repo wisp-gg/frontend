2ae7470836c8ecd23fa0d20f9a2d565ba1c26e01
Mason Rogers <mason@viction.dev>
2022-02-14 21:33:47 +0000

    refactor: server index can use more generic slots

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,20 +1,12 @@
 <template>
-    <list service-id="servers@getAll" :per-page="12">
-        <template #layout="list">
-            <div class="darker-input">
-                <input class="input w-full mb-8" name="search" :placeholder="t('generic.search')" @keyup="list.search">
-            </div>
-
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-6" v-if="list.results.length > 0">
-                <server-card v-for="(result, idx) of list.results" :key="idx" :server="result" />
+    <list service-id="servers@getAll" :per-page="12" searchable>
+        <template #results="{ results }">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-6" v-if="results.length > 0">
+                <server-card v-for="(result, idx) of results" :key="idx" :server="result" />
             </div>
             <div v-else>
                 <!-- TODO: better looking no items text :v -->
                 <t path="generic.no_items" />
-            </div>
-
-            <div class="flex justify-center mt-8">
-                <paginator :pagination-data="list.pagination" :set-page="list.setPage" />
             </div>
         </template>
     </list>
