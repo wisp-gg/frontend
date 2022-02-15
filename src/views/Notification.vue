9aef51a5876926bc970f03e8c5354fbe68f13d75
Mason Rogers <mason@viction.dev>
2022-02-15 18:24:24 +0000

    fix: validate notification url

--- src/views/Notification.vue
+++ src/views/Notification.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="p-2 pl-3" :class="`notification-${notification.type}`">
         <!-- TODO: if notification is clicked on, mark itself as read and possibly close the notification menu? -->
-        <component :is="notification.url ? 'router-link' : 'div'" :to="{name: notification.url?.[0], params: notification.url?.[1]}">
+        <component :is="url ? 'router-link' : 'div'" :to="{name: url?.[0], params: url?.[1]}">
             <div class="mb-1 text-white">
                 <fa :icon="['fas', notification.icon]" />
                 <span class="ml-1">
@@ -26,6 +26,7 @@
 
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
+import { useRouter } from 'vue-router';
 import { formatDateRelative } from '~/core';
 import { Notification } from '~/api/models';
 
@@ -37,9 +38,28 @@ export default defineComponent({
         },
     },
 
-    setup(props, context) {
+    setup(props) {
+        const router = useRouter();
+
         return {
             formatDateRelative,
+
+            url: computed(() => {
+                if (!props.notification.url) return;
+
+                try { // If any notification has an invalid url, the frontend is completely bricked unless you read notifications via api - this avoids that.
+                    router.resolve({
+                        name: props.notification.url[0],
+                        params: props.notification.url[1]
+                    });
+                } catch {
+                    console.log('invalid url');
+
+                    return;
+                }
+
+                return props.notification.url;
+            }),
 
             message: computed(() => {
                 return ['notifications.' + props.notification.message?.[0], props.notification.message?.[1]];
