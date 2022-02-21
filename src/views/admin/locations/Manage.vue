ab6e2789816b113bb073fd44e064274ab7438713
Stepan Fedotov <stepan.fedotov@gmail.com>
2022-02-21 13:25:45 +0200

    feat: clickable node name in location list, closes #90

--- src/views/admin/locations/Manage.vue
+++ src/views/admin/locations/Manage.vue
@@ -9,6 +9,12 @@
             <div class="w-full lg:w-1/2">
                 <container no-padding class="mb-4" title="admin.locations.nodes">
                     <list :service-id="location?.nodes || []" :fields="listFields">
+                        <template #field-name="{ result }">
+                            <v-button :to="{ name: 'admin.management.nodes.manage.about', params: { node: result.id } }" permission="node.read" class="text-white/75">
+                                {{ result.name }}
+                            </v-button>
+                        </template>
+
                         <template #field-fqdn="{ result }">
                             <code v-clipboard="result.connection.fqdn">
                                 {{ result.connection.fqdn }}
