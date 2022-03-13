8d3bad225c361dec61b899570bc52ae65a4847a1
havasuit <havasuited@gmail.com>
2022-03-13 17:30:16 +0000

    fix: More appropriate form collapsing on mobile in settings

--- src/views/admin/settings/General.vue
+++ src/views/admin/settings/General.vue
@@ -1,15 +1,15 @@
 <template>
     <container title="admin.settings.general.panel_settings">
         <v-form service-id="settings@updateGeneral">
-            <div class="flex flex-col space-y-4 lg:flex-row lg:space-x-4 lg:space-y-0">
-                <div class="lg:w-1/3">
+            <div class="flex flex-wrap">
+                <div class="xl:w-1/3 pr-6">
                     <v-input name="branding.name" footer="admin.settings.general.company_name_footer" :value="settings?.branding?.name" />
                     <v-switch name="misc.databases_allow_random" footer="admin.settings.general.random_database_deployment_footer" :value="settings?.misc?.databases_allow_random" />
                 </div>
-                <div class="lg:w-1/3">
+                <div class="xl:w-1/3 lg:w-1/2">
                     <v-radio-group name="misc.required_2fa" prefix="admin.settings.general.require_2fa" :options="['not_required', 'admin_only', 'all_users']" footer="admin.settings.general.require_2fa_footer" :value="settings?.misc?.required_2fa" />
                 </div>
-                <div class="lg:w-1/3">
+                <div class="xl:w-1/3 lg:w-1/2 pl-6">
                     <v-select name="default_locale" prefix="generic.languages" footer="admin.settings.general.default_language_footer" :options="languages" rule="required" :value="settings?.default_locale" />
                 </div>
             </div>
