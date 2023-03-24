234ef7ef7355f526225a6f503ed38c95b1849499
Jack Jenkins <j+git@ckjenkins.com>
2023-03-24 19:23:06 +0000

    Utility script for running production build locally

---
+++ run-prod.sh
@@ -0,0 +1,17 @@
+read -p "Do you want to rebuild? [y/n]" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   npm run build
+fi
+
+mkdir dist/assets 2> /dev/null && mv dist/panel dist/assets
+
+if ! grep -q BaseURL "dist/index.html"; then
+   source .env
+   sed -i "1i<script>\n    window.Wisp = {Debug: true, Node: \'local\', Version: \'dev\', BaseURL: \'$VITE_PANEL_URL\' };\n</script>\n" dist/index.html
+fi
+
+cd dist
+python3 -m http.server
+cd ../
