241ff0f05a1d3b0ebfee99a9b5b0e08787c3b934
Alex Filimonov <admin@softwarenoob.com>
2023-09-06 01:01:14 +0300

    feat: tooltip for POPs

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,9 @@
 <template>
     <footer>
         <div class="w-full text-center py-5">
-            <fa :icon="['fas', 'code-branch']" /> {{ version }}
+            <span v-tippy="setTippyContent" :data-content="nodeDescription">
+                <fa :icon="['fas', 'code-branch']" /> {{ version }}
+            </span>
         </div>
     </footer>
 </template>
@@ -10,9 +12,36 @@
 import { defineComponent, computed } from 'vue';
 
 export default defineComponent({
-    setup(props, context) {
+    setup() {
+        const version = computed(() => `${window.Wisp.Node || 'unknown'} - ${window.Wisp.Version || 'unknown'}`);
+
+        type NodeIdentifierToLocation = {
+            [key: string]: string;
+        };
+
+        const nodeIdentifierMap: NodeIdentifierToLocation  = {
+            'nl-ams': 'Netherlands, Amsterdam',
+            'au-syd': 'Australia, Sydney',
+            'fr-gra': 'France, Gravelines',
+            'sg-sin': 'Singapore, Singapore',
+            'ca-mnt': 'Canada, Montreal',
+            'local': 'local dev'
+        };
+
+        const node = window.Wisp.Node || 'unknown';
+        const nodeName = node.split('-web')[0];
+        const location = nodeIdentifierMap[nodeName] || 'Unknown Location';
+
+        const nodeDescription = `You are geo-routed to our web server in ${location}. We've automatically chosen this location to give you the fastest experience possible.`;
+
+        const setTippyContent = (el) => {
+            return el.dataset.content || 'Unknown';
+        };
+
         return {
-            version: computed(() => `${window.Wisp.Node || 'unknown'} - ${window.Wisp.Version || 'unknown'}`),
+            version,
+            nodeDescription,
+            setTippyContent
         };
     }
 });
