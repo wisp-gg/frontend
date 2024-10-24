fe3bc85f46639f51d742288097efcee5deb3803f
robertdennis <robertdennispersonal@gmail.com>
2024-10-24 19:46:18 +0900

    reset store and refresh

--- src/api/services/request.ts
+++ src/api/services/request.ts
@@ -110,7 +110,7 @@ export default class RequestService {
 
                 return res.data;
             })
-            .catch(err => {
+            .catch(async err =>  {
                 if (err instanceof TranslatableError) {
                     throw err;
                 }
@@ -154,7 +154,9 @@ export default class RequestService {
                         // TODO: this should be re-evaluated, if the session becomes invalid it'll be met with 401 (incl. settings endpoint => please refresh page)
                         // We can't refresh here because it'll trigger an infinite loop (unless @me endpoint gets whitelisted).
                         // Though I don't think this is really needed either.
-                        // refresh();
+                        await dispatch('user/set', undefined);
+
+                        refresh();
                         break;
                     case 403:
                         // TODO: display that no perms (although this should never realistically happen)
